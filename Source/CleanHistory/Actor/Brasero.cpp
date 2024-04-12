4c8270f738863a61032f277454613d6825486cfa
Cryp3rone <Manach.Nicolas.mn@gmail.com>
2024-04-12 17:18:35 +0200

    FixAgain

--- Source/CleanHistory/Actor/Brasero.cpp
+++ Source/CleanHistory/Actor/Brasero.cpp
@@ -1,7 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Brasero.h"
-#include "../ABurnable.h"
 #include "Components/BoxComponent.h"
 #include "Components/StaticMeshComponent.h"
 
