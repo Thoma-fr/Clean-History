9f6ed9b0cf51b0cf26c0bdc4e6a6b4ae6542206d
Cryp3rone <Manach.Nicolas.mn@gmail.com>
2024-05-11 14:21:23 +0200

    Bucket

---
+++ Source/CleanHistory/Interfaces/IWashableBroom.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "IWashableBroom.h"
+
+// Add default functionality here for any IIWashableBroom functions that are not pure virtual.
+
+void IIWashableBroom::CleanBroom()
+{
+}
