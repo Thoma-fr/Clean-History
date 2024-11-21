d74a2b3423b39031f83d62da56a91c89cbe43a4b
Cryp3rone <Manach.Nicolas.mn@gmail.com>
2024-11-21 17:35:30 +0100

    ToDebug scoring

--- Source/CleanHistory/Subsystem/ScoringSubSystem.cpp
+++ Source/CleanHistory/Subsystem/ScoringSubSystem.cpp
@@ -1,1 +1,145 @@
 #include "ScoringSubSystem.h"
+#include "Components/WidgetComponent.h"
+#include "Components/TextBlock.h" 
+#include "Kismet/GameplayStatics.h" 
+
+void UScoringSubSystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+
+	lastType = EScoringTypeEnum::DEFAULT;
+}
+
+void UScoringSubSystem::Deinitialize()
+{
+	Super::Deinitialize();
+}
+
+void UScoringSubSystem::Tick(float DeltaSeconds)
+{
+	comboTime += DeltaSeconds;
+	if (comboTime >= comboDelay)
+	{
+		comboTime = 0;
+		multiplier = 1;
+	}
+}
+
+void UScoringSubSystem::Score(EScoringTypeEnum scoreType, FVector WorldLocation)
+{
+	if (lastType != scoreType)
+	{
+		comboTime = 0;
+		multiplier += scoreTypeMultiplier[scoreType];
+	}
+
+	switch (scoreType)
+	{
+	case EScoringTypeEnum::DESTRUCTION:
+		scoreDestruction += scoreTypeValues[scoreType] * multiplier;
+		break;
+
+	case EScoringTypeEnum::CUT:
+		scoreCut += scoreTypeValues[scoreType] * multiplier;
+		break;
+
+	case EScoringTypeEnum::BLOOD:
+		scoreBlood += scoreTypeValues[scoreType] * multiplier;
+		break;
+
+	case EScoringTypeEnum::BURN:
+		scoreBurn += scoreTypeValues[scoreType] * multiplier;
+		break;
+
+	case EScoringTypeEnum::DEFAULT:
+		return;
+
+	default:
+		return;
+	}
+
+	DisplayScoreFeedback(scoreTypeValues[scoreType], WorldLocation);
+}
+
+void UScoringSubSystem::DisplayScoreFeedback(float Score, FVector WorldLocation)
+{ 
+	if (ScoreFeedbackWidgetClass) 
+	{ 
+		AActor* SpawnedActor = GetWorld()->SpawnActor<AActor>(ScoreFeedbackWidgetClass, WorldLocation, FRotator::ZeroRotator);
+		if (SpawnedActor)
+		{ 
+			UWidgetComponent* WidgetComp = SpawnedActor->FindComponentByClass<UWidgetComponent>();
+
+			if (WidgetComp)
+			{
+				UUserWidget* ScoreFeedbackWidget = WidgetComp->GetUserWidgetObject();
+				if (ScoreFeedbackWidget)
+				{
+					UTextBlock* ScoreText = (UTextBlock*)ScoreFeedbackWidget->GetWidgetFromName(TEXT("ScoreText"));
+					if (ScoreText)
+					{
+						ScoreText->SetText(FText::AsNumber(Score));
+					}
+
+					if (multiplier > 1)
+					{
+						UTextBlock* MultiplierText = (UTextBlock*)ScoreFeedbackWidget->GetWidgetFromName(TEXT("MultiplierText"));
+						if (MultiplierText)
+						{
+							MultiplierText->SetText(FText::AsNumber(multiplier));
+						}
+					}
+				}
+
+				/*FTimerHandle WidgetTimerHandle;
+				GetWorld()->GetTimerManager().SetTimer(WidgetTimerHandle, FTimerDelegate::CreateLambda([ScoreFeedbackWidget]()
+					{
+						if (ScoreFeedbackWidget)
+						{
+							ScoreFeedbackWidget->RemoveFromParent();
+						}
+					}), 2.0f, false);*/
+			}
+		} 
+	} 
+}
+
+int UScoringSubSystem::GetFinalScore()
+{
+	return scoreDestruction + scoreBlood + scoreCut + scoreBurn;
+}
+
+int UScoringSubSystem::GetScore(EScoringTypeEnum scoreType)
+{
+	switch (scoreType)
+	{
+	case EScoringTypeEnum::DESTRUCTION :
+		return scoreDestruction;
+		break;
+
+	case EScoringTypeEnum::CUT :
+		return scoreCut;
+		break;
+		
+	case EScoringTypeEnum::BLOOD :
+		return scoreBlood;
+		break; 
+
+	case EScoringTypeEnum::BURN :
+		return scoreBurn;
+		break;
+
+	case EScoringTypeEnum::DEFAULT :
+		return -1;
+		break;
+
+	default:
+		return -1;
+		break;
+	}
+}
+
+void UScoringSubSystem::SetComboDelay(float delay)
+{
+	comboDelay = delay;
+}
--- Source/CleanHistory/Subsystem/ScoringSubSystem.h
+++ Source/CleanHistory/Subsystem/ScoringSubSystem.h
@@ -1,5 +1,79 @@
 #pragma once
-class ScoringSubSystem
+
+#include "Subsystems/Subsystem.h"
+#include "Blueprint/UserWidget.h" 
+#include "ScoringSubSystem.generated.h"
+
+UENUM(BlueprintType)
+enum class EScoringTypeEnum : uint8
 {
+	DESTRUCTION,
+	CUT,
+	BLOOD,
+	BURN,
+	DEFAULT
 };
 
+UCLASS(Blueprintable)
+class UScoringSubSystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+public:
+
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+
+	virtual void Deinitialize() override;
+
+	virtual void Tick(float DeltaSeconds);
+
+	void DisplayScoreFeedback(float Score, FVector WorldLocation);
+
+	UFUNCTION(BlueprintCallable)
+	void Score(EScoringTypeEnum scoreType, FVector WorldLocation);
+
+	UFUNCTION(BlueprintCallable)
+	int GetFinalScore();
+
+	UFUNCTION(BlueprintCallable)
+	int GetScore(EScoringTypeEnum scoreType);
+
+	UFUNCTION(BlueprintCallable)
+	void SetComboDelay(float delay);
+
+private:
+	
+public:
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	TMap<EScoringTypeEnum, float> scoreTypeValues;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	TMap<EScoringTypeEnum, float> scoreTypeMultiplier;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float comboDelay;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	TSubclassOf<AActor> ScoreFeedbackWidgetClass;
+
+private: 
+	UPROPERTY()
+	int scoreDestruction = 0;
+
+	UPROPERTY()
+	int scoreCut = 0;
+
+	UPROPERTY()
+	int scoreBlood = 0;
+
+	UPROPERTY()
+	int scoreBurn = 0;
+
+	UPROPERTY()
+	float multiplier = 1;
+
+	UPROPERTY()
+	EScoringTypeEnum lastType;
+	
+	UPROPERTY()
+	float comboTime = 0;
+};
